249586fd131ceaa167bb11d86d30a803fabc4c87
Dischan <admin@dischan.ru>
2022-12-11 13:18:35 +0300

    ctcp client fixed

--- src/ctcp_client.h
+++ src/ctcp_client.h
@@ -22,7 +22,28 @@ _connection* ctcp_allocate_connection()
 	return con;
 }
 
+_connection* ctcp_setup_connection(_connection* con)
+{
+	size_t guid_offset = sizeof(_guid) / 2;
+
+	con->guid		  = ((_guid)rand() << guid_offset) + ((_guid)rand());
+	con->ping_last	  = time(NULL);
+	con->index_next   = 0u;
+	con->is_active	  = true;
+	con->encryption   = false;
+	con->is_logged_in = true;
+
+	memset(&con->key.pub, 0, sizeof(con->key.pub));
+	memset(&con->key.priv, 0, sizeof(con->key.priv));
+
+	memset(&con->input, 0, sizeof(con->input));
+	memset(&con->output, 0, sizeof(con->output));
+
+	return con;
+}
+
 void ctcp_dispose_connection(_connection* con)
 {
-	free(con);
+	memset(con, 0, sizeof(*con));
+	//free(con);
 }
